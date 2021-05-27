bf7fde059905f756e825ea520b487d5e5dffdfe1
Marin Živković <marin.zivkovic@subnet.hr>
2021-05-27 19:40:08 +0200

    UserUserDtoAutoMapperProfiles

---
+++ eBeertijaBackend/eBeertijaBackend/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using ebeertijaBackend.DTOs;
+using ebeertijaBackend.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ebeertijaBackend.Helpers
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<User, UserDto>();
+            CreateMap<UserDto, User>();
+        }
+    }
+}
