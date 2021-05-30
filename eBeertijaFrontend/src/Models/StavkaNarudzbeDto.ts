3865e319f5d8d50dc7290a008362400f27b8315d
Eni Rabar <erabar@veleri.hr>
2021-05-30 18:28:12 +0200

    stavka_racuna_module

---
+++ eBeertijaFrontend/src/Models/StavkaNarudzbeDto.ts
@@ -0,0 +1,8 @@
+export class StavkaNarudzbeDto {
+    narudzbaId?: number;
+    stavkaCjenikaId?: number;
+    jedinicnaCijenaStavke?: number;
+    nazivStavke?: string;
+    kolicina?: number;
+    ukupno?: number;
+}
